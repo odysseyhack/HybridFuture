e47cbf9e2f6564e918b6885b94c2a9ba3c072ccf
Evert0x <evert@hatching.io>
2019-04-13 17:32:42 +0200

    Initial dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_basicauth import BasicAuth
 import settings
 
@@ -10,11 +10,30 @@ app.config['BASIC_AUTH_PASSWORD'] = settings.BASIC_PASSWORD
 basic_auth = BasicAuth(app)
 
 
+class DATA:
+    MESSAGES_SINCE_START = 0
+
+
 @app.route('/', methods=['POST'])
 @basic_auth.required
-def secret_view():
+def index():
     r = request.json
     d = {
-        "fulfillmentText": "hehhe"
+        "fulfillmentText": "hehe"
     }
-    return "", 200
+    DATA.MESSAGES_SINCE_START += 1
+    return jsonify(d), 200
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html", data=DATA.MESSAGES_SINCE_START)
+
+
+@app.route('/dashboard/data')
+def dashboard_data():
+    return jsonify(
+        {
+            "messages": DATA.MESSAGES_SINCE_START
+        }
+    )
